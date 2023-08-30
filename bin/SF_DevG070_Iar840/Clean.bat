3c6c36d7016d59c9c69cab4d8150c64c17876b57
Dongwen Gan <dwgan@foxmail.com>
2023-08-30 14:39:18 +0800

    Fix Initial Current Value bug, update 'Clean.bat' and deleted unecessary file

---
+++ bin/SF_DevG070_Iar840/Clean.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM 遍历当前目录中的所有文件
+for %%i in (*) do (
+    REM 检查文件扩展名，如果不是 .ewp, .eww 或 .bat 则删除
+    if "%%~xi" neq ".ewp" (
+        if "%%~xi" neq ".eww" (
+            if "%%~xi" neq ".bat" (
+                del "%%i"
+            )
+        )
+    )
+)
+
+REM 删除所有子文件夹
+for /d %%i in (*) do (
+    rmdir /s /q "%%i"
+)
+
+endlocal
+echo All done.
+pause
